8f4d55f25d27d501928dfb013a3bbd2e0c7bc0c3
honeyjangra2309 <54791179+honeyjangra2309@users.noreply.github.com>
2021-09-06 18:53:51 +0530

    Add files via upload

---
+++ canvas.js
@@ -0,0 +1,103 @@
+const canvas=document.getElementById('canvas');
+canvas.width=window.innerWidth-60;
+canvas.height=400;
+
+let context=canvas.getContext('2d');
+let start_background_color="white";
+context.fillStyle=start_background_color;
+context.fillRect(0,0,canvas.width,canvas.height);
+
+let draw_color="black";
+let draw_width="2";
+let is_drawing=false;
+
+let restore_array=[];
+let index=-1;
+
+canvas.addEventListener('touchstart',start,false);
+canvas.addEventListener('touchmove',draw,false);
+canvas.addEventListener('mousedown',start,false);
+canvas.addEventListener('mousemove',draw,false);
+
+canvas.addEventListener('touchend',stop,false);
+canvas.addEventListener('mouseup',stop,false);
+canvas.addEventListener('mouseout',stop,false);
+
+function start(event) {
+    is_drawing=true;
+    context.beginPath();
+    context.moveTo(event.clientX-canvas.offsetLeft,event.clientY-canvas.offsetTop);
+    event.preventDefault();
+}
+
+function draw(event) {
+    if(is_drawing)
+    {
+        context.lineTo(event.clientX-canvas.offsetLeft,event.clientY-canvas.offsetTop)
+    
+    context.strokeStyle=draw_color;
+    context.lineWidth=draw_width;
+    context.lineCap="round";
+    context.lineJoin="round";
+    context.stroke();
+    }
+    event.preventDefault();
+}
+function stop(event) {
+  if(is_drawing)
+  {
+      context.stroke();
+      context.closePath();
+      is_drawing=false;
+  }
+  event.preventDefault();
+  if(event.type != 'mouseout') {
+  restore_array.push(context.getImageData(0,0,canvas.width,canvas.height))
+  index+=1; }
+ // console.log(restore_array);
+}
+function change_color(element) {
+    draw_color=element.style.background;
+}
+
+function clear_canvas() {
+    context.fillStyle=start_background_color;
+    context.clearRect(0,0,canvas.width,canvas.height);
+    context.fillRect(0,0,canvas.width,canvas.height);
+
+    restore_array=[];
+    index=-1;
+}
+
+let redo_array=[];
+let redo_index=-1;
+function undo_last() {
+  if(index<=0)
+  {
+      clear_canvas();
+  }
+  else{
+      index-=1;
+     let redo=restore_array.pop();
+     redo_array.push(redo);
+     redo_index+=1;
+     console.log(redo_array);
+      context.putImageData(restore_array[index],0,0);
+  }
+}
+function redo_last() {
+    context.putImageData(redo_array[redo_index],0,0);
+    redo_index-=1;
+    let undo=redo_array.pop();
+    restore_array.push(undo);
+    //restore_array.push(redo_array[redo_index]);
+    index+=1;
+}
+ var download=document.getElementById('download');
+ download.addEventListener('click',function(e) {
+     var link=document.createElement('a');
+     link.download='download.png';
+     link.href=canvas.toDataURL();
+     link.click();
+     link.delete;
+ })
